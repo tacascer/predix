eb0ebfc3bc02acbc156e2da02764e330f33d71c2
Tim Tran <tdt46@cornell.edu>
2022-09-23 14:49:32 -0400

    Initial commit

---
+++ src/main/java/github/com/tacascer/predix/PredixApplication.java
@@ -0,0 +1,13 @@
+package github.com.tacascer.predix;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PredixApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PredixApplication.class, args);
+	}
+
+}
---
+++ src/test/java/github/com/tacascer/predix/PredixApplicationTests.java
@@ -0,0 +1,13 @@
+package github.com.tacascer.predix;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PredixApplicationTests {
+
+	@Test
+	void contextLoads() {
+
+	}
+}
